3cf97b377cafed74a52a103e2956393c2f4e9ce3
Jacob Millner <jacob.millner@outlook.com>
2022-05-08 18:33:49 -0400

    json marshalling

--- bank/main.go
+++ bank/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")	
-	})
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipCode"`
+}
 
+func main() {
+	http.HandleFunc("/greet", handleGreet)
+	http.HandleFunc("/customers", handleCusomters)
+
+	fmt.Println("Listening on Port 8000")
 	http.ListenAndServe("localhost:8000", nil)
+}
+
+func handleGreet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+func handleCusomters(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"Rob", "New Delhi", "110075"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
