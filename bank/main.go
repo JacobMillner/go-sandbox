be752cfb9d2aa03784bd3a09db4aad636697a1ca
Jacob Millner <jacob.millner@outlook.com>
2022-05-08 19:23:24 -0400

    add xml marshalling

--- bank/main.go
+++ bank/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zipCode"`
+	Name    string `json:"name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipCode" xml:"zipCode"`
 }
 
 func main() {
@@ -29,6 +30,12 @@ func handleCusomters(w http.ResponseWriter, r *http.Request) {
 		{"Ashish", "New Delhi", "110075"},
 		{"Rob", "New Delhi", "110075"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
