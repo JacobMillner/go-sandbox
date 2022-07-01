0b78e8adcf360db6aab656b826027841249818ed
Jacob Millner <jacob.millner@outlook.com>
2022-07-01 15:21:17 -0400

    add id and use unsafe interfaces

--- go-scraper/main.go
+++ go-scraper/main.go
@@ -4,21 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 type grammar struct {
+	Id         string `json:"id"`
 	Eng        string `json:"eng"`
 	Jp         string `json:"jp"`
 	Definition string `json:"def"`
 	Link       string `json:"link"`
 }
 
+func generateId(id string) string {
+	newId := strings.TrimSpace(id)
+	newId = strings.ReplaceAll(newId, " ", "-")
+	newId = strings.ToLower(newId)
+	return newId
+}
+
 func main() {
 	visitedLinks := make(map[string]string)
 	var linksToVisit []string
-	var grammarList []grammar
+	var grammarList []interface{}
 
 	c := colly.NewCollector(colly.AllowedDomains("jlptsensei.com"))
 	c.OnRequest(func(r *colly.Request) {
@@ -28,13 +37,18 @@ func main() {
 		fmt.Println(r.StatusCode)
 	})
 	c.OnHTML(".jl-row", func(e *colly.HTMLElement) {
+		id := generateId(e.ChildText(".jl-td-gr"))
 		newGrammar := grammar{
+			Id:         id,
 			Eng:        e.ChildText(".jl-td-gr"),
 			Jp:         e.ChildText(".jl-td-gj"),
 			Definition: e.ChildText(".jl-td-gm"),
 			Link:       e.ChildAttr("td > .jl-link", "href"),
 		}
-		grammarList = append(grammarList, newGrammar)
+		var grammarTupel []interface{}
+		grammarTupel = append(grammarTupel, newGrammar.Id)
+		grammarTupel = append(grammarTupel, newGrammar)
+		grammarList = append(grammarList, grammarTupel)
 	})
 	c.OnHTML(".page-numbers", func(e *colly.HTMLElement) {
 		if e.Attr("href") != "" {
@@ -55,7 +69,7 @@ func main() {
 	}
 
 	// time to output the grammar!
-	file, _ := json.Marshal(grammarList)
+	file, _ := json.MarshalIndent(grammarList, "", "\t")
 	fmt.Println(string(file))
 
 	_ = ioutil.WriteFile("test.json", file, 0644)
